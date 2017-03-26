8670c65b36d4f05bef7fbab057232903b03b6fdb
gempir <daniel.pasch.s@gmail.com>
2017-03-26 12:36:26 +0200

    Initial Commit

---
+++ traymusic/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  traymusic
+//
+//  Created by Daniel Pasch-Sannapiu on 26/03/2017.
+//  Copyright © 2017 gempir. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
